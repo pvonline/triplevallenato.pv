d914adba8d5bfbd63d267092553c9055e27e3be3
lucaspalaciopimienta <radioelpalaciovallenato@gmail.com>
2024-12-05 16:04:03 -0500

    luta

---
+++ js/app.js
@@ -0,0 +1,71 @@
+var swiper = new Swiper('.swiper-container', {
+	navigation: {
+	  nextEl: '.swiper-button-next',
+	  prevEl: '.swiper-button-prev'
+	},
+	slidesPerView: 1,
+	spaceBetween: 10,
+	// init: false,
+	pagination: {
+	  el: '.swiper-pagination',
+	  clickable: true,
+	},
+
+  
+	breakpoints: {
+	  620: {
+		slidesPerView: 1,
+		spaceBetween: 20,
+	  },
+	  680: {
+		slidesPerView: 2,
+		spaceBetween: 40,
+	  },
+	  920: {
+		slidesPerView: 3,
+		spaceBetween: 40,
+	  },
+	  1240: {
+		slidesPerView: 4,
+		spaceBetween: 50,
+	  },
+	} 
+    });
+
+/*-------------------------------------------*/
+
+
+
+
+
+/*---------- triciclo de slider----------*/
+var swiper = new Swiper(".mySwiper", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    loop: true,
+    autoplay:{
+        delay: 2500,
+        disableOnInteraction: false,
+        pauseOnMouseEnter: true, //This will pause autoplay when the slider is hovered (sin la coma)
+    },
+    coverflowEffect: {
+      rotate: 50,
+      stretch: -10,
+      depth: 100,
+      modifier: 1,
+      slideShadows: true,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    }
+  });
+
+  /¨------------¨/
